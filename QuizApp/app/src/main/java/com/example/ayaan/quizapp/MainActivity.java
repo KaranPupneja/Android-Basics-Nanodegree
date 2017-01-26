0db86379e5ae8d1cd442aaf160d9bc8e8a46ff67
Ayaan Faiz <ayaan2103@gmail.com>
2017-01-26 16:13:24 +0530

    Third Project

--- QuizApp/app/src/main/java/com/example/ayaan/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/ayaan/quizapp/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.ayaan.quizapp;
-
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,26 +7,14 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import static com.example.ayaan.quizapp.R.string.question1;
-
 public class MainActivity extends AppCompatActivity {
-
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        }
-
+    }
     int score = 0;
     int selectdid;
-
     public void finalscore(View view) {
         float f = 0, f1 = 0, f2 = 0, f3 = 0;
         RadioGroup clickd = (RadioGroup) findViewById(R.id.ques1);
@@ -66,49 +52,8 @@ public class MainActivity extends AppCompatActivity {
         }
         f = f1 + f2 + f3 + question4() + question5();
         Toast.makeText(this, "Your score is :" + f, Toast.LENGTH_SHORT).show();
-
     }
-
     String s;
-
-    //    public int question1(){
-//        RadioGroup clickd = (RadioGroup)findViewById(R.id.ques1);
-//        selectdid = clickd.getCheckedRadioButtonId();
-//        RadioButton selectedradio = (RadioButton)findViewById(selectdid);
-//        s= (String) selectedradio.getText();
-//        if(s.equals("Virat Kohli"))
-//        {
-//            score = 5;
-//        }
-//        else
-//            score = 0;
-//        return score;
-//    }
-//    public int question2() {
-//        RadioGroup clickd = (RadioGroup) findViewById(R.id.ques2);
-//        int selectdid = clickd.getCheckedRadioButtonId();
-//        RadioButton selectedradio = (RadioButton) findViewById(selectdid);
-//
-//        String s1 = (String) selectedradio.getText();
-//        if (s1.equals("Donald Trump")) {
-//            score = 5;
-//        } else
-//            score = 0;
-//        return score;
-//    }
-//
-//    public int question3() {
-//        RadioGroup clickd = (RadioGroup) findViewById(R.id.ques3);
-//        int selectdid = clickd.getCheckedRadioButtonId();
-//        RadioButton selectedradio = (RadioButton) findViewById(selectdid);
-//        String s2 = (String) selectedradio.getText();
-//        if (s2.equals("Canberra")) {
-//            score = 5;
-//        } else
-//            score = 0;
-//        return score;
-//    }
-
     public int question4() {
         EditText clickd = (EditText) findViewById(R.id.ques4);
         String s3 = clickd.getText().toString();
@@ -119,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
             score = 0;
         return score;
     }
-
     public float question5() {
         CheckBox froyo = (CheckBox) findViewById(R.id.froyo);
         CheckBox honeycomb = (CheckBox) findViewById(R.id.honey);
@@ -138,9 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 d = (float) (d + 2.5);
             if (ho == true)
                 d = (float) (d + 2.5);
-
         }
         return d;
     }
 }
-
